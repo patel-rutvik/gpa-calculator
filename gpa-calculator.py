fa57c19cf4c0dec85a51f54da3e3fa1bf757262c
Rutvik Patel <RutvikPatel@Rutviks-MacBook-Pro.local>
2019-01-01 16:50:19 -0700

    turned off debugging output

--- gpa-calculator.py
+++ gpa-calculator.py
@@ -1,7 +1,7 @@
 # Simple GPA Calculator ran through terminal
 # Made by: Rutvik Patel
 
-debug = True
+debug = False
 scale = {"CR": 0, "A+": 4.0, "A": 4.0, "A-": 3.7, "B+": 3.3, "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0, "C-": 1.7, "D+": 1.3, "D": 1.0}
 
 # get input
